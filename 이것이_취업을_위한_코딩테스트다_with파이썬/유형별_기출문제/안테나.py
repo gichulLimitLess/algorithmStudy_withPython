ad46052b367e287db176eadc53729bb350372a48
Junho Heo <hanascreen@konkuk.ac.kr>
2025-09-30 00:02:47 +0900

    feat: 유형별 기출문제 풀이 - 정렬(1문제) / 아이디어를 내는 것이 중요함(2)

--- 이것이_취업을_위한_코딩테스트다_with파이썬/유형별_기출문제/안테나.py
+++ 이것이_취업을_위한_코딩테스트다_with파이썬/유형별_기출문제/안테나.py
@@ -1,1 +1,16 @@
 # 안테나
+# --> 집에다가 안테나를 설치, 동일한 위치에 여러 집 존재 가능
+# --> 안테나로부터 모든 집까지의 거리 총 합이 최소가 되도록
+# 해결법? 오름차순으로 정렬해서, 가운데에 있는 친구를 선택하면 될 듯
+# --> len(house_list) // 2 하면.. mid에서 가장 작은 값으로 출력됨
+
+n = int(input()) # 집의 수 n개
+house_list = list(map(int, input().split()))
+
+house_list.sort() # 오름차순 정렬
+if len(house_list) % 2 == 0: # 길이가 짝수라면
+  idx = len(house_list) // 2 - 1 # 더 작은 애를 출력해야 하므로, 인덱스 -1 해야 함
+else: # 홀수면 
+  idx = len(house_list) // 2
+
+print(house_list[idx]) # 이게 그냥 정답임
