bea0a6a94129fa8354a51b89a711e42854d9d1e4
Junho Heo <hanascreen@konkuk.ac.kr>
2025-10-06 12:01:57 +0900

    feat: 백준 11726번 풀이 - DP는 행동의 분류로 접근하라 / 서로 겹치지 않게 쪼개는 행동의 기준을 세우는 것

--- 문제풀이/Silver/BOJ_11726_2xn 타일링.py
+++ 문제풀이/Silver/BOJ_11726_2xn 타일링.py
@@ -3,11 +3,11 @@ n = int(input())
 
 dp = [0 for _ in range(n+1)]
 
-dp[0] = 0
-dp[1] = 1
-dp[2] = 2
-
-for i in range(3, n+1): # dp 테이블 채워 나가기
-    dp[i] = (dp[i-1] + dp[i-2]) % 10007
-
-print(dp[n]) # 결과 출력
+if n == 1:
+    print(1)
+else:
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1): # dp 테이블 채워 나가기
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    print(dp[n]) # 결과 출력ㄴ
