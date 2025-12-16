7d76246f7538eb52c1f78e2cfcba2a0f75c56156
Junho Heo <hanascreen@konkuk.ac.kr>
2025-12-17 08:23:04 +0900

    feat: 백준 11468번 풀이 - 시간복잡도 계산 조금 더 세밀하게 해봐도 좋을 듯 / 문자열 슬라이싱 연산은 다른거에 비해 매우 빠르다는 것을 기억

--- 문제풀이/Silver/BOJ_11478_서로 다른 부분 문자열의 개수.py
+++ 문제풀이/Silver/BOJ_11478_서로 다른 부분 문자열의 개수.py
@@ -9,6 +9,7 @@ substrings = set() # 서로 다른 것의 부분 문자열 세기 위해, 여기
 # 여기서.. O(N^3) 소요
 for length in range(1, len(s)+1):
     for start in range(len(s)-length+1):
-        substrings.add(s[start:start+length]) # 여기서 O(N) 소모
+        # 여기서 O(N) 소모 --> 그치만, 슬라이싱 연산은 C 레벨의 메모리 복사 연산이라 다른거에 비해 매우 빠름
+        substrings.add(s[start:start+length])
 
 print(len(substrings)) # 서로 다른 부분 문자열 개수 출력
